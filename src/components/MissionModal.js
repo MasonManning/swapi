587e62ecbb45e57a9f12791b88969fd2edbf96e3
MasonManning <MasonManning@protonmail.com>
2020-06-26 23:50:40 +1000

    Display starships for selection in MissionModal

--- src/components/MissionModal.js
+++ src/components/MissionModal.js
@@ -1,22 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import {Button, Modal} from 'react-bootstrap'
+import {Context} from '../UserContext' 
 function MissionModal(){
  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  const context = useContext(Context)
+  console.log(context)
+    const StarshipSelection = context.userData.starships.map(item => {
+        return (<h1>{item.name}</h1>)
+    })
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+          Accept Mission
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Starship Selection</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Please select starships:</Modal.Body>
+        <Modal.Body>Please select starships:
+            <br/>
+            {StarshipSelection}
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
