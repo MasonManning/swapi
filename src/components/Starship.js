23e28a17cf7b6cf1a495a0715f80c0a1e8c1988c
Mason Manning <masonmanning@protonmail.com>
2020-08-13 18:13:13 +1000

    Add next and previous buttons to the Starship component
    Add the ability to load the next page and the previous page of Starships

--- src/components/Starship.js
+++ src/components/Starship.js
@@ -4,15 +4,20 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import StarshipItem from "./StarshipItem"
 import CaptainContext from "../CaptainContext"
+import Button from 'react-bootstrap/Button'
 class Starship extends Component {
     constructor() {
         super()
         this.state = {
             starships: [],
             isLoading: true,
+            next: "",
+            previous: "", 
         }
         this.handleCptChange = this.handleCptChange.bind(this)
         this.handleCptSubmit = this.handleCptSubmit.bind(this)
+        this.handleNext = this.handleNext.bind(this)
+        this.handlePrevious= this.handlePrevious.bind(this)
     }
     handleCptChange(event) {
         this.setState({ captainName: event.target.value })
@@ -21,15 +26,60 @@ class Starship extends Component {
         alert("Welcome Captain " + this.state.captainName)
         event.preventDefault()
     }
+    handleNext(event) {
+       if(this.state.next){
+        fetch(this.state.next)
+            .then(res => res.json())
+            .then(data => {
+                this.setState(ps => {
+                    const list = []
+                    console.log(data)
+                    data.previous ? console.log("there is a previous" ) : console.log("Previous is null")
+                    // console.log(data.previous)
+                    return ({
+                        starships: data.results,
+                        isLoading: false,
+                        previous: data.previous ? data.previous : '',
+                        next: data.next ? data.next : ''
+                    })
+                })
+            })
+       } 
+    }
+    handlePrevious(event){
+        if(this.state.previous){
+        fetch("https://swapi.dev/api/starships/")
+            .then(res => res.json())
+            .then(data => {
+                this.setState(ps => {
+                    const list = []
+                    console.log(data)
+                    data.previous ? console.log("there is a previous" ) : console.log("Previous is null")
+                    // console.log(data.previous)
+                    return ({
+                        starships: data.results,
+                        isLoading: false,
+                        previous: data.previous ? data.previous : '',
+                        next: data.next ? data.next : ''
+                    })
+                })
+            })
+        }
+    }
     componentDidMount() {
         fetch("https://swapi.dev/api/starships/")
             .then(res => res.json())
             .then(data => {
                 this.setState(ps => {
                     const list = []
+                    console.log(data)
+                    data.previous ? console.log("there is a previous" ) : console.log("Previous is null")
+                    // console.log(data.previous)
                     return ({
                         starships: data.results,
-                        isLoading: false
+                        isLoading: false,
+                        previous: data.previous ? data.previous : '',
+                        next: data.next ? data.next : ''
                     })
                 })
             })
@@ -47,6 +97,8 @@ class Starship extends Component {
                 <Row>
                     {!this.state.isLoading && starshipsItem}
                 </Row>
+                <Button onClick={this.handleNext}>Next</Button>
+                <Button onClick={this.handlePrevious}>Previous</Button>
             </div>
         )
     }
