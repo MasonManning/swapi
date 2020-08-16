2658fd7596a5744ddafbe845a3f630da7c3e7670
Mason Manning <masonmanning@protonmail.com>
2020-08-16 23:53:38 +1000

    Update Save component UI to use bootstrap

--- src/components/Save.js
+++ src/components/Save.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Db from '../StorageManagement'
 import { UserConsumer } from '../UserContext'
 import StateItem from './StateItem'
+import Button from 'react-bootstrap/Button'
 const Save = () => {
     return (
 
-        <UserConsumer>
+        < UserConsumer >
             {
                 consumer => {
                     //Call getSaved from theStorageManagement to get a list of all saved files.
@@ -13,20 +14,24 @@ const Save = () => {
                     return (
                         <div>
                             {savedList && console.log("Display Saved Items by maping over the savedList and outputting the StyateItem component")}
-                            {savedList && savedList.map((i,index) => <StateItem data={i} key={i.saveId} index={index}/> )}
+                            {savedList && savedList.map((i, index) => <StateItem data={i} key={i.saveId} index={index} />)}
                             {savedList && console.log(savedList)}
 
-                            <div onClick={(e) => {
+                            {/* <div onClick={(e) => {
                                 const hasSaved = Db.getInstance().newSave(consumer.userData)
-                                hasSaved && savedList.push(<StateItem data={hasSaved} key={hasSaved.saveId} index={savedList.length+1} />)
+                                hasSaved && savedList.push(<StateItem data={hasSaved} key={hasSaved.saveId} index={savedList.length + 1} />)
 
-                            }} style={{ height: "150px" }}><h1>NEW SAVE</h1></div>
+                            }} style={{ height: "150px" }}><h1>NEW SAVE</h1></div> */}
+                            <Button onClick={() => {
+                                const hasSaved = Db.getInstance().newSave(consumer.userData)
+                                hasSaved && savedList.push(<StateItem data={hasSaved} key={hasSaved.saveId} index={savedList.length + 1} />)
+                            }}>Save</Button>
                             <h1>Save Comp: {consumer.userData.username}</h1>
                         </div>
                     )
                 }
             }
-        </UserConsumer>
+        </UserConsumer >
     )
 }
 export default Save
